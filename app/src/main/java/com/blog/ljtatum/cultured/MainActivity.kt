5b787e4d3afff7f596648549d445ac929a724f7d
drxeno02 <ljtatum@hotmail.com>
2020-07-04 11:57:59 -0700

    initial commit

---
+++ app/src/main/java/com/blog/ljtatum/cultured/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.blog.ljtatum.cultured
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
